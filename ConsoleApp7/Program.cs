b35936cfdd67f3a380d5e72f58c50e435771e1f1
Imod23 <imodpankaja76@gmail.com>
2024-03-23 17:30:11 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(001, "Nimal", "Gampaha", 56);
+            Student student2 = new Student(002, "Nimali", "Gampola", 66);
+
+
+            student1.Address = "Kandy";
+            Console.WriteLine(student1.HasPassed());
+            Console.WriteLine(student2.HasPassed());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp7/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Student
+    {
+
+        public int StudentIndex { get; set; }
+        public string StudentName { get; set; }
+
+        public string Address { get; set; }
+
+        public int StudentMark { get; set; }
+
+        public Student(int aStudentIndex, string aStudentName, string aAddress, int aStudentMark)
+        {
+            StudentIndex = aStudentIndex;
+            StudentName = aStudentName;
+
+            Address = aAddress;
+            StudentMark = aStudentMark;
+
+        }
+
+
+        public bool HasPassed()
+        {
+            if (StudentMark >= 60)
+            {
+                return true;
+
+            }
+
+
+            return false;
+
+        }
+
+
+
+
+    }
+}
